cf17cbd6d2805df37a71ae527d195cf31e8aa120
ThiagoHBA <thiagohhab@gmail.com>
2021-09-26 13:20:21 -0300

    adicionando algoritmo de votação para decisão do resultado da classe

--- predict_cnn_model.py
+++ predict_cnn_model.py
@@ -9,15 +9,17 @@ import time
 
 cam = cv2.VideoCapture(0)
 
-
 def main():
     fileModelX = FileModel('exp_349_x', 'model_struct.json', 'model_weights_299.h5')
     fileModelY = FileModel('exp_335_y', 'model_struct.json', 'model_weights_299.h5')
-    
     fileDataset = FileDataset('datasets','sidewalk_accy_all_datasets_classes_new_1630_00', 'dronet', '000001.jpg')
-
+    
     modelX = compileModel(fileModelX)
     modelY = compileModel(fileModelY)
+    
+    limitPrediction = 3
+    counterPredict = 0
+    classPredictions = []
     
     while True:
         resizedImage = captureAndResizedImage(fileDataset.getImageSize(), cv2.COLOR_BGR2GRAY)
@@ -28,8 +30,16 @@ def main():
         classX = modelX.predict(resizedImage, batch_size=64)
         classY = modelY.predict(resizedImage, batch_size=64)
 
-        print("Image in class X: {}".format(np.argmax(classX)))
-        print("Image in class Y: {}".format(np.argmax(classY)))
+        if(counterPredict == limitPrediction):
+            print(classPredictions)
+            print("Image in class X: {}".format(classVote(classPredictions, 'x')))
+            print("Imagem in class Y: {}".format(classVote(classPredictions, 'y')))
+
+            classPredictions = []
+            counterPredict = 0
+        else:
+            classPredictions.append((np.argmax(classX), np.argmax(classY)))
+            counterPredict += 1
         
 
 def compileModel(fileModel):
@@ -42,18 +52,33 @@ def compileModel(fileModel):
         
     return _model
 
-def captureAndResizedImage(imageSize, colorScale):   
+def captureAndResizedImage(imageSize, colorScale):
     ret, camImage = cam.read()
     cv2.imshow('Imagem', camImage)
-
-    cv2.waitKey(100)
+    cv2.waitKey(166) # ~6 frames per second
     camImage = cv2.resize(camImage, imageSize)
     camImage = cv2.cvtColor(camImage, colorScale)
-
     x = image.img_to_array(camImage)
     x = np.expand_dims(x, axis=0)
 
     return x
+
+def classVote(classValues, direction):
+  directionIndex = 1 if direction.lower() == 'y' else 0
+  values = []
+  for i in range(len(classValues)):
+    values.append(classValues[i][directionIndex])
+
+  highestOccurrency = 0
+  selectedClass = values[-1]
+
+  for occurrency in range(len(values)-1, -1 , -1):
+    counterOccurrency = values.count(values[occurrency])
+    if(counterOccurrency > highestOccurrency):
+      highestOccurrency = counterOccurrency
+      selectedClass = values[occurrency]
+
+  return selectedClass
    
 if __name__ == "__main__":
     main()
