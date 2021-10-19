e19556ee1eddf26ca1c58a26b92bdb3a25144484
ThiagoHBA <thiagohhab@gmail.com>
2021-10-19 19:12:15 -0300

    corrigindo valor das classes no enum de classificação

--- common/enum/classification_enum.py
+++ common/enum/classification_enum.py
@@ -1,15 +1,15 @@
 from enum import Enum
 
 class LinearClassificationClass(Enum):
-    URGENT_STOP = 4
-    REDUCE_A_LOT = 3
+    URGENT_STOP = 0
+    REDUCE_A_LOT = 1
     SLIGHTLY_REDUCE = 2
-    KEEP_SPEED = 1
-    SPEED_UP = 0
+    KEEP_SPEED = 3
+    SPEED_UP = 4
 
 class SidesClassificationClass(Enum):
-    STRONG_SIDESTEP_LEFT = 4
-    SLIGHTLY_SIDESTEP_LEFT = 3
+    STRONG_SIDESTEP_LEFT = 0
+    SLIGHTLY_SIDESTEP_LEFT = 1
     NO_SIDESTEP = 2
-    SLIGHT_SIDESTEP_RIGHT = 1
-    STRONG_SIDESTEP_RIGHT = 0
+    SLIGHTLY_SIDESTEP_RIGHT = 3
+    STRONG_SIDESTEP_RIGHT = 4
