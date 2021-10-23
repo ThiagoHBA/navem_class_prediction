9f8e017902b9cf56970e50c244a52f4df58f510b
ThiagoHBA <thiagohhab@gmail.com>
2021-10-23 17:22:53 -0300

    removendo comentários inuteis

--- predict_cnn_model.py
+++ predict_cnn_model.py
@@ -6,19 +6,16 @@ import cv2
 cam = cv2.VideoCapture(0)
 
 def main():
-    '''Configurations'''
     limitPredictions  = 3
     infinity = True
     showMetrics = False
-    path = 'D:/Documentos/datasets/2020_06_25-16_03_09/'
+    path = ''
     datasetArchitecture = DatasetArchitectureUtil('dronet')
 
-    '''Load Models'''
     modelX = FileModel('models/exp_349_x', 'model_struct.json', 'model_weights_299.h5').compileModel()
     modelY = FileModel('models/exp_335_y', 'model_struct.json', 'model_weights_299.h5').compileModel()
 
-    '''Start Predict Process'''
-    ClassificationUtil(limitPredictions, datasetArchitecture, modelX, modelY, cam, path, infinity, showMetrics).filePredictProcess(750)
+    ClassificationUtil(limitPredictions, datasetArchitecture, modelX, modelY, cam, path, infinity, showMetrics).realTimeLoopProcess()
        
 if __name__ == "__main__":
     main()
